d6ea6f28880a4dc2d872e388b3a759f0925ce9cd
Dyandra Allen <dallen9@binghamton.edu>
2018-10-13 16:38:45 -0400

    Initial Commit

---
+++ flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flashcards
+//
+//  Created by Dyandra Allen on 10/13/18.
+//  Copyright © 2018 Dyandra Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
